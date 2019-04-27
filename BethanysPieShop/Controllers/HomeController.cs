fb36b665185b9d34a3aad2a87e1a0997941b3461
Talha Liaquat <talhaliaquat@gmail.com>
2019-04-27 14:32:04 +0400

    Basic Pie Home M V C

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        
+        //Constructor Injection
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie Overview";
+
+            var pies = pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            return View(pies);
+        }
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,20 +16,26 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //AddTransient: Whenever someone asked for IPieRepo, a new MockPieRepo will be returned (new instance).
+            //AddSingleton: Same instance will be returned whenver requeted
+            //AddScoped: Same intance for same request, different requested will have different instances
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
+
+
+
+            //Register framework service
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //Sequence is important
+            app.UseDeveloperExceptionPage();//Display exception page
+            app.UseStatusCodePages(); //Display 404 etc
+            app.UseStaticFiles(); //route for static file in wwwroot
+            app.UseMvcWithDefaultRoute(); //use MVC default route
         }
     }
 }
